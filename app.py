8279d6e27e8557a70e9d819e82f38bbba77467c6
CastilloDevX <al075041@uacam.mx>
2025-10-04 23:00:20 -0600

    feat: :art: Extension for team colab

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/view/')
+def view_planet():
+    return render_template('info.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
